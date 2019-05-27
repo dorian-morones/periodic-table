66b141f448e99d343ec8ebfcf5d359795c900cc3
Dorian Morones <dorian.morones@nxp.com>
2019-05-26 21:11:54 -0500

    first draft
    simplete react implementation with local data

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/styles/main.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import createHistory from 'history/createBrowserHistory'
-import { routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
 
-const history = createHistory()
-const middleware = [routerMiddleware(history), thunk];
-
-ReactDOM.render(
-        <App history={history} />,
-
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
